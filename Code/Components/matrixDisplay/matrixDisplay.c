9bee32830f7c227e442fe52fa5b5aaab86f9f5d9
Demetri <demetriv.s.7@gmail.com>
2024-10-31 23:34:44 -0700

    Added function for display solid graphic

--- Code/Components/matrixDisplay/matrixDisplay.c
+++ Code/Components/matrixDisplay/matrixDisplay.c
@@ -6,6 +6,11 @@
 #include "matrixDisplay.h"
 #include "prvGraphics.h"
 
+
+/*-----------------------------------------------------------
+Literal Constants
+------------------------------------------------------------*/
+
 #define CASCADE_SIZE 5  // Number of cascaded MAX7219 modules in a display
 #define NUM_DISPLAYS 2  // Number of displays in the system
 
@@ -15,6 +20,11 @@
 #define CLK_PIN 12
 
 #define MATRIX_DISP_TAG "matrix_display"
+
+
+/*-----------------------------------------------------------
+Types
+------------------------------------------------------------*/
 
 typedef struct
 {
@@ -29,30 +39,92 @@ typedef struct
 } cursor_t;
 
 
+/*-----------------------------------------------------------
+Statics
+------------------------------------------------------------*/
 static matrixDisplay_t lowerDisplay;
 static matrixDisplay_t upperDisplay;
 
 static matrixDisplayPtr_t displays[NUM_DISPLAYS] = {&lowerDisplay, &upperDisplay};
 
+
+/*-----------------------------------------------------------
+Local Function Prototypes
+------------------------------------------------------------*/
+
+/*
+* Description:
+*   Displays a graphic accross all displays
+*     
+*   NOTE:
+*      Must contain enough frames to fill all segments
+*      Graphic is displayed starting from the far left 
+*      of the top display and moves to the right
+*
+* Arguments:
+*      uint64_t *graphic: Pointer to graphic array
+*      int size: Size of the graphic array
+*
+* Returns:
+*      esp_err_t: ESP_OK if the graphic was displayed successfully
+*/
+esp_err_t displayFullGraphic(const uint64_t *graphic, const int size);
+
+/*-----------------------------------------------------------
+Functions
+------------------------------------------------------------*/
+
 void clearDisplay(display_t display)
 {
-switch (display)
-{
-case LOWER_DISPLAY:
-    max7219_clear(&displays[LOWER_DISPLAY]->dev);
-    break;
-case UPPER_DISPLAY:
-    max7219_clear(&displays[UPPER_DISPLAY]->dev);
-    break;
-case ALL_DISPLAYS:
-    max7219_clear(&displays[LOWER_DISPLAY]->dev);
-    max7219_clear(&displays[UPPER_DISPLAY]->dev);
-    break;
+    switch (display)
+    {
+    case LOWER_DISPLAY:
+        max7219_clear(&displays[LOWER_DISPLAY]->dev);
+        break;
+    case UPPER_DISPLAY:
+        max7219_clear(&displays[UPPER_DISPLAY]->dev);
+        break;
+    case ALL_DISPLAYS:
+        max7219_clear(&displays[LOWER_DISPLAY]->dev);
+        max7219_clear(&displays[UPPER_DISPLAY]->dev);
+        break;
 
-default:
-    ESP_LOGE(MATRIX_DISP_TAG, "Invalid display");
-    break;
+    default:
+        ESP_LOGE(MATRIX_DISP_TAG, "Invalid display");
+        break;
+    }
 }
+
+esp_err_t displayFullGraphic(const uint64_t *graphic, const int size)
+{
+    int frame = 0;
+    esp_err_t ret = ESP_OK;
+
+    // Check if pointer is valid
+    if(graphic == NULL)
+    {
+        ESP_LOGE(MATRIX_DISP_TAG, "Invalid graphic pointer");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    // Check if there are enough frames to fill all segments
+    if(size / sizeof(uint64_t) < NUM_DISPLAYS * CASCADE_SIZE)
+    {
+        ESP_LOGE(MATRIX_DISP_TAG, "Not enough frames to fill all segments");
+        return ESP_ERR_INVALID_SIZE;
+    }
+
+    // Display starting from the top display
+    for(int display = NUM_DISPLAYS - 1; display >= 0; display--)
+    {
+        for(uint8_t segment = 0; segment < CASCADE_SIZE; segment++)
+        {
+            ret |= max7219_draw_image_8x8(&displays[display]->dev, segment*8, &graphic[frame]);
+            frame++;
+        }
+    }
+
+    return ret;
 }
 
 esp_err_t display_init(void)
@@ -96,7 +168,7 @@ esp_err_t display_init(void)
     for(size_t frame = 0; frame < sizeof(bootAnimation) / sizeof(uint64_t); frame++)
     {
         for(size_t segment = 0; segment < CASCADE_SIZE; segment++)
-    {
+        {
             ESP_ERROR_CHECK(max7219_draw_image_8x8(&lowerDisplay.dev, segment*8, &bootAnimation[frame]));
             ESP_ERROR_CHECK(max7219_draw_image_8x8(&upperDisplay.dev, segment*8, &bootAnimation[frame]));
         }
@@ -107,6 +179,8 @@ esp_err_t display_init(void)
 
     // Clear the displays
     clearDisplay(ALL_DISPLAYS);
+
+    ESP_ERROR_CHECK(displayFullGraphic(wordNSeek, sizeof(wordNSeek)));
 
     return ESP_OK;
 }
